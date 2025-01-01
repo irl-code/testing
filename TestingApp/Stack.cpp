9741fc4f9adc517c4d43363456fe5b3d8371c9e2
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2025-01-01 22:43:47 +0500

    [Features] added some new features.

--- TestingApp/Stack.cpp
+++ TestingApp/Stack.cpp
@@ -14,4 +14,12 @@ class Stack {
 public:
     Stack(const int size): capacity(size), top(-1), stack(new int[capacity]){}
     bool isEmpty() const {return top == -1;}
+    void Resize(){
+        capacity += capacity * 2;
+        int* new_stack = new int[capacity];
+        for(int i = 0; i <= top; ++i)
+            new_stack[i] = stack[i];
+        delete [] stack;
+        stack = new_stack;
+    }
 };
