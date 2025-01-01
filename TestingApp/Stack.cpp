0b5a06bb88c985d6f17aeac5fe3ec5479b2f6319
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2025-01-01 21:10:28 +0500

    [Feature] added the new file of stack in C++

---
+++ TestingApp/Stack.cpp
@@ -0,0 +1,16 @@
+//
+//  Stack.cpp
+//  TestingApp
+//
+//  Created by Hamza Wahab on 01/01/2025.
+//
+
+#include <iostream>
+
+class Stack {
+    int capacity;
+    int top;
+    int* stack;
+public:
+    Stack(const int size): capacity(size), top(-1), stack(new int[capacity]){}
+};
