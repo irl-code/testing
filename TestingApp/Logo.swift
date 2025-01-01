f812c1fa629b20ebb2c42095df78ee76f3c7b945
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2025-01-01 21:14:45 +0500

    [Feature] uncommented the I love you text.
    (cherry picked from commit 6b162e154e2b457413e25ca7105e91fba592a451)

--- TestingApp/Logo.swift
+++ TestingApp/Logo.swift
@@ -10,7 +10,51 @@ import SwiftUI
 struct Logo: View {
     @State var isLove: Bool = false
     var body: some View {
-        Text("Hello world!")
+        VStack{
+            Button {
+                isLove.toggle()
+            } label: {
+                Image(systemName: "house.fill")
+                    .resizable()
+                    .frame(width: 50, height: 45)
+                    .foregroundStyle(.red)
+            }
+            Button{
+                // code
+            } label: {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundStyle(.orange)
+                    .font(.title)
+            }
+            .padding()
+            Button("Click here") {
+                // code
+            }
+            .foregroundStyle(Color.green)
+            .font(.title)
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(Color.gray.opacity(0.4))
+            
+                .overlay(
+                    ZStack{
+                       if isLove {
+                            Text("I Love You!")
+                                .foregroundStyle(Color.red)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
+                                .shadow(color: Color.red.opacity(1.0), radius: 30, x: 5, y: -5)
+                                .shadow(color: Color.red.opacity(1.0), radius: 30, x: -5, y: 5)
+                        }
+                    }
+                    
+                        // code
+                )
+            
+                .padding()
+        }
+        .padding(.top, 20)
+        
     }
 }
 
