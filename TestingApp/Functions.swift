c42a8fedc84a4f87a1a76e2f90fb6d7ad541fcaa
Hamza Wahab <96471443+irl-code@users.noreply.github.com>
2025-01-04 09:18:59 +0500

    hw/changing-color (#2)
    [Feature] added new Swift file Functions and made some changes to the Circle file.

---
+++ TestingApp/Functions.swift
@@ -0,0 +1,16 @@
+//
+//  Functions.swift
+//  TestingApp
+//
+//  Created by Hamza Wahab on 04/01/2025.
+//
+
+import Foundation
+
+var name: String = "Hamza Wahab"
+var age: Int = 20
+
+func getNameAndAge() -> (String, Int) {
+    return (name, age)
+}
+
