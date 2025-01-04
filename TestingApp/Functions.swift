a9881391c8983cc961c2d00c4828e79a3cfc8a48
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2025-01-04 09:05:06 +0500

    [feature] added the  new file functions and write getName function to return name qnd age.

---
+++ TestingApp/Functions.swift
@@ -0,0 +1,14 @@
+//
+//  Functions.swift
+//  TestingApp
+//
+//  Created by Hamza Wahab on 04/01/2025.
+//
+
+import Foundation
+
+var name: String = "Hamza Wahab"
+
+func getName() -> (String, Int) {
+    return (name, 24)
+}
